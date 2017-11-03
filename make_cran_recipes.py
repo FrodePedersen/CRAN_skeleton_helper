51da85297bc431f5ca9c170cdc4f6b2513206bb8
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2017-11-03 16:07:13 +0100

    finished implementing skeleton_helper

--- make_cran_recipes.py
+++ make_cran_recipes.py
@@ -10,11 +10,10 @@ def main():
                 print("Could not find {} on cran".format(line))
 
 def handleGithub(package):
-    sp.call('git pull upstream master', shell=True)
-    sp.call('git checkout -b ' + package)
-    sp.call('git add recipes/' + package)
-    sp.call('git commit -m \"added ' + package + '\"')
-    sp.call('git push origin ' + package)
+    sp.call('git checkout -b ' + package, shell=True)
+    sp.call('git add recipes/' + package, shell=True)
+    sp.call('git commit -m \"added ' + package + '\"', shell=True)
+    sp.call('git push origin ' + package, shell=True)
 
 
 if __name__=="__main__":
