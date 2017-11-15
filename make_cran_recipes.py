c700f3bba02542f4e9a438167a43c542dc934ab8
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2017-11-15 18:08:15 +0100

    cran_skeleton_helper should obey pep8

--- make_cran_recipes.py
+++ make_cran_recipes.py
@@ -1,21 +1,24 @@
 import skeleton_helper as sh
 import subprocess as sp
+
+
 def main():
     with open('packages_to_process.txt', 'r') as file:
         for line in file.readlines():
             try:
                 sh.write_recipe(line[2:-1], 'recipes')
-                handleGithub(line)
+                handle_github(line)
             except FileNotFoundError as e:
                 print("Could not find {} on cran".format(line))
 
-def handleGithub(package):
+
+def handle_github(package):
     sp.call('git checkout -b ' + package, shell=True)
     sp.call('git add recipes/' + package, shell=True)
     sp.call('git commit -m \"added ' + package + '\"', shell=True)
     sp.call('git push origin ' + package, shell=True)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
